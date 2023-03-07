55b27c2f2863c48e06d9e7734d55d85f4829c560
chanakanissanka <40533683+chanakanissanka@users.noreply.github.com>
2023-03-08 10:11:30 +1300

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>!!!Hello Welcome you all to Azure Core Services!!! V2</h1></body></html>\n"
+    return "<html><body><h1>!!!Hello Welcome you all to Azure Core Services!!! V3</h1></body></html>\n"
